77b60b1b4cd448ecf623bdaef4301a11056683e8
AdrianLinares <adrianlinares246@protonlmail.com>
2026-04-22 12:10:09 -0500

    fix: correct casing of Index.css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './Index.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
+=======
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './Index.css';
+
+createRoot(document.getElementById('root')!).render(<App />);
+>>>>>>> e5d3247 (fix: correct casing of Index.css import)
